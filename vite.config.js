6ed923e1a305f463ded5c0e0abdc479a51e63796
FaberGG <jhonfabernavia@gmail.com>
2024-10-26 14:54:27 -0500

    cambios para el yml

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
-  base: "https://FaberGG.github.io/SimuladorFisica/",
+  base: "/",
   server: {
     // Esta opción asegura que cualquier 404 se maneje devolviendo `index.html`
     historyApiFallback: true,
